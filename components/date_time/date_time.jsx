a4fe0c0627244322f21e494821f42ca264f2075f
arijitpatra <ari.clicks@gmail.com>
2018-01-19 00:50:53 +0530

    HLS video streaming

--- components/date_time/date_time.jsx
+++ components/date_time/date_time.jsx
@@ -7,7 +7,11 @@ class DateTime extends React.Component {
 
         var today = new Date(),
             date = today.getDate() + '-' + (today.getMonth() + 1) + '-' + today.getFullYear(),
-            time = today.toLocaleTimeString();
+            time = today.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'});
+
+        if(today.getMonth() == 0){
+        	date = 'Jan ' + today.getDate() + ', ' + today.getFullYear();
+        }
 
         this.state = {
             date: date,
@@ -18,7 +22,9 @@ class DateTime extends React.Component {
 	render(){
 		return(
 			<div className="date_time">
-				{this.state.date} {this.state.time}
+				<b>{this.state.time}</b>
+				<span> </span>
+				<i>{this.state.date}</i>
 			</div>
 		);
 	}
--- components/video/video.jsx
+++ components/video/video.jsx
@@ -6,10 +6,45 @@ import DateTime from '../date_time/date_time.jsx';
 import Menu from '../Menu/menu.jsx';
 
 class Video extends React.Component {
+
+	componentDidMount() {
+		//configuring support for HLS 
+		if(Hls.isSupported()) {
+		    var video = document.getElementById('hls-video');
+		    var hls = new Hls();
+		    hls.loadSource('https://video-dev.github.io/streams/x36xhzz/x36xhzz.m3u8');
+		    hls.attachMedia(video);
+		    hls.on(Hls.Events.MANIFEST_PARSED,function() {
+		      video.play();
+		    });
+		}
+		// hls.js is not supported on platforms that do not have Media Source Extensions (MSE) enabled.
+		// When the browser has built-in HLS support (check using `canPlayType`), we can provide an HLS manifest (i.e. .m3u8 URL) directly to the video element throught the `src` property.
+		// This is using the built-in support of the plain video element, without using hls.js.
+		else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+		    video.src = 'https://video-dev.github.io/streams/x36xhzz/x36xhzz.m3u8';
+		    video.addEventListener('canplay',function() {
+		      video.play();
+		    });
+		}
+	}
+
 	render(){
+
+		{/*const video_source = "http://media.w3.org/2010/05/bunny/movie.mp4";*/}
+
+		const playerResolutions = {
+			height: 1080,
+			width: 1920
+		}
+
 		return(
 			<div className="video">
-				<video src="../assets/100.mp4" type="video/mp4" muted autoPlay loop>
+				{/*<video src="../assets/100.mp4" type="video/mp4" muted autoPlay loop>*/}
+				{/*<video src={video_source} type="video/mp4" height="1080" width="1920" autoPlay loop>*/}
+				<video id="hls-video" style={playerResolutions} muted autoPlay loop>
+					{/*FALLBACK*/}
+					<p>No support for HTML5 video tag in the browser!</p>
 				</video>
 				<Overlay/>
 				<BrandLogo/>
