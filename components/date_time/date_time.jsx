6c168274e0880ef37276b0e1482a2583d35043f7
Arijit Patra <ari.clicks@gmail.com>
2018-02-02 19:44:17 +0530

    Update date_time.jsx

--- components/date_time/date_time.jsx
+++ components/date_time/date_time.jsx
@@ -2,29 +2,67 @@ import React from 'react';
 import './date_time.scss';
 
 class DateTime extends React.Component {
-	constructor() {
-        super();
 
-        var today = new Date(),
-            date = today.getDate() + '-' + (today.getMonth() + 1) + '-' + today.getFullYear(),
-            time = today.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'});
-
-        if(today.getMonth() == 0){
-        	date = 'Jan ' + today.getDate() + ', ' + today.getFullYear();
-        }
-
-        this.state = {
-            date: date,
-            time: time
-        };
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
     }
 
+    componentDidMount() {
+        this.timerID = setInterval(
+          () => this.tick(),
+          1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+          date: new Date()
+        });
+    }
+
+
 	render(){
+
+        var display_time = this.state.date.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'});
+        var display_date;
+        
+        //logic for month names
+        if (this.state.date.getMonth() == 0) {
+            display_date = 'Jan ' + this.state.date.getDate() + ', ' + this.state.date.getFullYear()
+        } else if (this.state.date.getMonth() == 1) {
+            display_date = 'Feb ' + this.state.date.getDate() + ', ' + this.state.date.getFullYear()
+        } else if (this.state.date.getMonth() == 2) {
+            display_date = 'Mar ' + this.state.date.getDate() + ', ' + this.state.date.getFullYear()
+        } else if (this.state.date.getMonth() == 3) {
+            display_date = 'Apr ' + this.state.date.getDate() + ', ' + this.state.date.getFullYear()
+        } else if (this.state.date.getMonth() == 4) {
+            display_date = 'May ' + this.state.date.getDate() + ', ' + this.state.date.getFullYear()
+        } else if (this.state.date.getMonth() == 5) {
+            display_date = 'Jun ' + this.state.date.getDate() + ', ' + this.state.date.getFullYear()
+        } else if (this.state.date.getMonth() == 6) {
+            display_date = 'Jul ' + this.state.date.getDate() + ', ' + this.state.date.getFullYear()
+        }  else if (this.state.date.getMonth() == 7) {
+            display_date = 'Aug ' + this.state.date.getDate() + ', ' + this.state.date.getFullYear()
+        } else if (this.state.date.getMonth() == 8) {
+            display_date = 'Sep ' + this.state.date.getDate() + ', ' + this.state.date.getFullYear()
+        } else if (this.state.date.getMonth() == 9) {
+            display_date = 'Oct ' + this.state.date.getDate() + ', ' + this.state.date.getFullYear()
+        } else if (this.state.date.getMonth() == 10) {
+            display_date = 'Nov ' + this.state.date.getDate() + ', ' + this.state.date.getFullYear()
+        } else if (this.state.date.getMonth() == 11) {
+            display_date = 'Dec ' + this.state.date.getDate() + ', ' + this.state.date.getFullYear()
+        } 
+
 		return(
 			<div className="date_time">
-				<b>{this.state.time}</b>
-				<span> </span>
-				<i>{this.state.date}</i>
+				<b>{display_time}</b>
+                <span> </span> 
+                <i>{display_date}</i>
 			</div>
 		);
 	}
